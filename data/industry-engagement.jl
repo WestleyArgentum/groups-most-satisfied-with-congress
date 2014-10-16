693e078dcfe0bb8f94707fd386ddd51478a9b636
WestleyArgentum <WestleyArgentum@gmail.com>
2014-10-16 10:50:22 -0400

    data and munging script

---
+++ data/industry-engagement.jl
@@ -0,0 +1,105 @@
+
+using JSON
+
+const industries_filename = "112th-industries.json"
+const bills_filename = "112th-bills.json"
+const output_filename = "industry-engagement.json"
+
+function filter_overlapping_votes(bills)
+    overlap = Any[]
+    for (k,v) in bills
+        for (k2,v2) in bills
+            if k != k2 && v["num"] == v2["num"] && v["prefix"] == v2["prefix"]
+                if !([k, k2] in overlap) && !([k2, k] in overlap)
+                    push!(overlap, [k, k2])
+                end
+            end
+        end
+    end
+
+    for (id1, id2) in overlap
+        (haskey(bills, id1) && haskey(bills, id2)) || continue
+
+        passed1 = get(bills[id1], "dateVote", -1)
+        passed2 = get(bills[id2], "dateVote", -2)
+        if passed1 == passed2
+            delete!(bills, id1)
+        end
+    end
+
+    bills
+end
+
+function filter_has_votes(bills)
+    filter((k,b)->get(b, "action", "") == "passage", bills)
+end
+
+function build_engagement_map(bills, industries)
+    engagement_map = Dict()
+
+    for (id, name) in industries
+        engagement_map[id] = {
+            "supported" => 0,
+            "opposed" => 0
+        }
+    end
+
+    for (aid, data) in bills
+        supporters = data["positions"]["support"]
+        for ind in supporters
+            engagement_map[ind]["supported"] += 1
+        end
+
+        opposers = data["positions"]["opposed"]
+        if "JE300" in opposers
+            println(opposers)
+        end
+        for ind in opposers
+            engagement_map[ind]["opposed"] += 1
+        end
+    end
+
+    engagement_map
+end
+
+function format_engagement_data(engagement_map)
+    engagement_table = [
+        { "name" => "Supported", "data" => Any[] },
+        { "name" => "Opposed", "data" => Any[] },
+    ]
+
+    supporter_data = engagement_table[1]["data"]
+    opposed_data = engagement_table[2]["data"]
+
+    for (ind, data) in engagement_map
+
+        push!(supporter_data, {
+            "x" => industries[ind],
+            "y" => data["supported"]
+        })
+
+        push!(opposed_data, {
+            "x" => industries[ind],
+            "y" => data["opposed"]
+        })
+    end
+
+    engagement_table
+end
+
+
+industries = JSON.parse(readall(industries_filename))
+bills = JSON.parse(readall(bills_filename))
+bills = filter_overlapping_votes(filter_has_votes(bills))
+
+for (aid, data) in bills
+    data["positions"]["support"] = unique(data["positions"]["support"])
+    data["positions"]["opposed"] = unique(data["positions"]["opposed"])
+end
+
+
+engagement_map = build_engagement_map(bills, industries)
+
+out = open(output_filename, "w")
+write(out, json(format_engagement_data(engagement_map)))
+close(out)
