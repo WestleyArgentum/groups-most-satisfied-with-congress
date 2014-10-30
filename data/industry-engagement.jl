ccf0c8a8df25bd606e5a82fd15daafd3d02a4572
WestleyArgentum <WestleyArgentum@gmail.com>
2014-10-29 23:16:33 -0400

    more flexibility to generate data for smaller graphs

--- data/industry-engagement.jl
+++ data/industry-engagement.jl
@@ -3,7 +3,10 @@ using JSON
 
 const industries_filename = "112th-industries.json"
 const bills_filename = "112th-bills.json"
-const output_filename = "industry-engagement.json"
+
+const output_total_positions = "industry-engagement.json"
+const output_support_positions = "top-supporters.json"
+const output_oppose_positions = "top-opposers.json"
 
 function filter_overlapping_votes(bills)
     overlap = Any[]
@@ -59,18 +62,27 @@ function build_engagement_map(bills, industries)
     engagement_map
 end
 
-function format_engagement_data(engagement_map)
+function format_engagement_data(engagement_map, sort_by, support_first = true)
     order = collect(keys(engagement_map))
-    sort!(order, lt = (lhs, rhs) -> ((engagement_map[lhs]["supported"] + engagement_map[lhs]["opposed"]) >
-                                    (engagement_map[rhs]["supported"] + engagement_map[rhs]["opposed"])))
+    sort!(order, lt = (lhs, rhs) -> sort_by(engagement_map, lhs, rhs))
 
-    engagement_table = [
-        { "name" => "Bills Supported by Group", "data" => Any[] },
-        { "name" => "Bills Opposed by Group", "data" => Any[] },
-    ]
+    if support_first
+        engagement_table = [
+            { "name" => "Bills Supported by Group", "data" => Any[] },
+            { "name" => "Bills Opposed by Group", "data" => Any[] }
+        ]
 
-    supporter_data = engagement_table[1]["data"]
-    opposed_data = engagement_table[2]["data"]
+        supporter_data = engagement_table[1]["data"]
+        opposed_data = engagement_table[2]["data"]
+    else
+        engagement_table = [
+            { "name" => "Bills Opposed by Group", "data" => Any[] },
+            { "name" => "Bills Supported by Group", "data" => Any[] }
+        ]
+
+        supporter_data = engagement_table[2]["data"]
+        opposed_data = engagement_table[1]["data"]
+    end
 
     for ind in order
         push!(supporter_data, {
@@ -89,6 +101,19 @@ function format_engagement_data(engagement_map)
     engagement_table
 end
 
+function sort_total_positions(engagement_map, lhs, rhs)
+    return ((engagement_map[lhs]["supported"] + engagement_map[lhs]["opposed"]) >
+        (engagement_map[rhs]["supported"] + engagement_map[rhs]["opposed"]))
+end
+
+function sort_support_positions(engagement_map, lhs, rhs)
+    return engagement_map[lhs]["supported"] > engagement_map[rhs]["supported"]
+end
+
+function sort_opposed_positions(engagement_map, lhs, rhs)
+    return engagement_map[lhs]["opposed"] > engagement_map[rhs]["opposed"]
+end
+
 
 industries = JSON.parse(readall(industries_filename))
 bills = JSON.parse(readall(bills_filename))
@@ -102,6 +127,14 @@ end
 
 engagement_map = build_engagement_map(bills, industries)
 
-out = open(output_filename, "w")
-write(out, json(format_engagement_data(engagement_map)))
+out = open(output_total_positions, "w")
+write(out, json(format_engagement_data(engagement_map, sort_total_positions)))
+close(out)
+
+out = open(output_support_positions, "w")
+write(out, json(format_engagement_data(engagement_map, sort_support_positions)))
+close(out)
+
+out = open(output_oppose_positions, "w")
+write(out, json(format_engagement_data(engagement_map, sort_opposed_positions, false)))
 close(out)
